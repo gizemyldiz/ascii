8789f7047a4480d6858dfb245dac029f48529f69
gizem yıldız <yildizgizemnet@outlook.com>
2020-04-11 01:44:51 +0300

    ödev dosyam

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace sifreOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1Sifre = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCumle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1Sifre
+            // 
+            this.button1Sifre.Location = new System.Drawing.Point(143, 114);
+            this.button1Sifre.Name = "button1Sifre";
+            this.button1Sifre.Size = new System.Drawing.Size(75, 23);
+            this.button1Sifre.TabIndex = 0;
+            this.button1Sifre.Text = "Şifre Çöz";
+            this.button1Sifre.UseVisualStyleBackColor = true;
+            this.button1Sifre.Click += new System.EventHandler(this.button1Sifre_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "..";
+            // 
+            // textBoxCumle
+            // 
+            this.textBoxCumle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxCumle.Location = new System.Drawing.Point(118, 45);
+            this.textBoxCumle.Name = "textBoxCumle";
+            this.textBoxCumle.Size = new System.Drawing.Size(100, 38);
+            this.textBoxCumle.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 356);
+            this.Controls.Add(this.textBoxCumle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1Sifre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1Sifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCumle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sifreOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1Sifre_Click(object sender, EventArgs e)
+        {
+            string sifreliMetin = coz(textBoxCumle.Text);
+            cozumGoster("Şifreli Metin:", sifreliMetin);
+        }
+        void cozumGoster(string sifreliMetin, string cozulecekMetin)
+        {
+            label1.Text = sifreliMetin;
+            label2.Text = cozulecekMetin;
+        }
+        string coz(string cozulecekMetin)
+        {
+
+            string cozulmusSonuc = "";
+            foreach (char karakter in cozulecekMetin)
+            {
+                int ascii = (int)karakter;
+                int yeniMetin = ascii - 1;
+                char yeniKarakter = (char)yeniMetin;
+                cozulmusSonuc += yeniMetin.ToString();
+
+
+            }
+            return cozulmusSonuc;
+        }
+    }
+}
